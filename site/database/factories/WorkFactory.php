afe9d47c2fe91d6a745d190b7d7229ef32ff4741
Shuichi Minamie <s.minamie@gmail.com>
2021-02-20 12:30:24 +0000

    Add Work all

---
+++ site/database/factories/WorkFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\work;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WorkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = work::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
