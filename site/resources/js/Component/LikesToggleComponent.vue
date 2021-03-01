b36a2f8d7c498c93b8eb05bd155fb61e674b4c4b
Shuichi Minamie <s.minamie@gmail.com>
2021-03-01 16:54:00 +0000

    fix works likes count

---
+++ site/resources/js/Component/LikesToggleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <span> {{ likes }}</span>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    model: String,
+    likesId: Number,
+  },
+  data() {
+    return {
+      likes: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("/like/toggle/" + this.model + "/" + this.likesId)
+      .then((response) => (this.likes = response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
